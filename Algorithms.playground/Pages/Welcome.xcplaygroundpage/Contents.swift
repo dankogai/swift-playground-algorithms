857245831f962555365cd4d0ba535564cce86761
Dan Kogai <dankogai+github@gmail.com>
2016-01-13 04:23:52 +0900

    update Pages/Welcome

--- Algorithms.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
@@ -1,9 +1,11 @@
 /*:
+
 # Welcome
 
 This document is written in comments, marked up by markdown.
 
 * https://developer.apple.com/library/ios/recipes/Playground_Help/Chapters/AboutPlaygrounds.html
 
+[Next](@next)
 
 */
