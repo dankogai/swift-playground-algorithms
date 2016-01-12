f7bcc34c7cf47d607f5ef2dd0ed7d54d0d48913b
Dan Kogai <dankogai+github@gmail.com>
2016-01-13 05:10:20 +0900

    + Exponentiation by squaring

--- Algorithms.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
@@ -2,10 +2,11 @@
 
 # Welcome
 
-This document is written in comments, marked up by markdown.
-
-* https://developer.apple.com/library/ios/recipes/Playground_Help/Chapters/AboutPlaygrounds.html
+* This document is written in comments, marked up by markdown.
+* Check the [About Playgrounds] web page by Apple to find how to use playground.
 
 [Next](@next)
+
+[About Playgrounds]: https://developer.apple.com/library/ios/recipes/Playground_Help/Chapters/AboutPlaygrounds.html
 
 */
