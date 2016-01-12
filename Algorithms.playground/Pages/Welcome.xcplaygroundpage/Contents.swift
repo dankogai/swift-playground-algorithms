0df913fbfac2d2cc2c693a4285994281c5d6b7cd
Dan Kogai <dankogai+github@gmail.com>
2016-01-13 04:13:37 +0900

    add Algorithms.playground

---
+++ Algorithms.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+/*:
+# Welcome
+
+This document is written in comments, marked up by markdown.
+
+* https://developer.apple.com/library/ios/recipes/Playground_Help/Chapters/AboutPlaygrounds.html
+
+
+*/
